f9e5fbfb6e384c729fe016b1e163b03197abc16c
Stewart ilondanga <stilondanga@gmail.com>
2019-01-19 11:05:56 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*var navigate = (function() {
+var navigate = (function() {
   $('.dd').toggle();
   $('.dd_btn').click(function() {
     var dataName = $(this).attr('data-name');
@@ -7,6 +7,6 @@
   });
 })();
 
-setTimeout(function() {
+/*setTimeout(function() {
   document.getElementsByTagName("input")[0].click();
 }, 1000);
